4f479f4d46606760973c4d9333b12dbc15ae6b41
SzenftnerGergely <gergely.szenftner@gmail.com>
2023-10-25 12:25:51 +0200

    icon, btn global style set up.

--- app/(components)/Nav.tsx
+++ app/(components)/Nav.tsx
@@ -1,10 +1,24 @@
+import { faTicket } from '@fortawesome/free-solid-svg-icons'
+import { faHome } from '@fortawesome/free-solid-svg-icons/faHome'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import Link from 'next/link'
 import React from 'react'
 
 const Nav = () => {
   return (
-    <div>
-      
-    </div>
+    <nav>
+        <div>
+            <Link href="/">
+                <FontAwesomeIcon icon={faHome} className='icon' />
+            </Link>
+            <Link href="/TicketPage/new">
+                <FontAwesomeIcon icon={faTicket} className='icon' />
+            </Link>
+        </div>
+        <div>
+            <p className="text-default-text">example@gmail.com</p>
+        </div>
+    </nav>
   )
 }
 
--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Daschboard = () => {
   return (
     <div>
-      
+      Dashboard
     </div>
   )
 }
