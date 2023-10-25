6436ca6070e86781b87b37dd9cbd7eb5f6258e00
SzenftnerGergely <gergely.szenftner@gmail.com>
2023-10-25 12:37:09 +0200

    DeleteBlock set up

--- app/(components)/DeleteBlock.tsx
+++ app/(components)/DeleteBlock.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import { faX } from '@fortawesome/free-solid-svg-icons/faX'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const DeleteBlock = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+        <FontAwesomeIcon icon={faX} className='text-red-400 hover:cursor-pointer hover:text-red-200' />
+    </>
   )
 }
 
