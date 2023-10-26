a6be69d1c0d046a91eb2ea4e6cc1eb9bcbff6dec
SzenftnerGergely <gergely.szenftner@gmail.com>
2023-10-26 15:40:53 +0200

    types Ticket created, PriorityDisplay comp props added

--- app/(components)/TicketCard.tsx
+++ app/(components)/TicketCard.tsx
@@ -1,25 +1,30 @@
+import { Ticket } from '../(models)/types'
 import DeleteBlock from './DeleteBlock'
 import PriorityDisplay from './PriorityDisplay'
 import ProgressDisplay from './ProgressDisplay'
 import StatusDisplay from './StatusDisplay'
 
-const TicketCard = () => {
+type TicketCardProps = {
+    ticket: Ticket
+}
+
+const TicketCard = ({ticket}: TicketCardProps) => {
     return (
         <div className='flex flex-col bg-card hover:bg-card-hover rounded-md shadow-lg p-3 m-2'>
             <div className='flex items-center mb-3'>
-                <PriorityDisplay />
+                <PriorityDisplay priority={ticket.priority} />
                 <div className='ml-auto'>
                     <DeleteBlock />
                 </div>
             </div>
 
-            <h4>Ticket Title</h4>
+            <h4>{ticket.title}</h4>
             <hr className='h-px border-0 bg-page mb-2' />
-            <p className='whitespace-pre-wrap'></p>
+            <p className='whitespace-pre-wrap'>{ticket.description}</p>
             <div className='flex-grow'></div>
             <div className='flex mt-2'>
                 <div className='flex flex-col'>
-                    <p className='text-xs my-1'>08/31.23</p>
+                    <p className='text-xs my-1'>{ticket.createdAt}</p>
                     <ProgressDisplay />
                 </div>
                 <div className='ml-auto flex items-end'>
---
+++ app/(models)/types.ts
@@ -0,0 +1,10 @@
+export type Ticket = {
+    title: string,
+    description: string,
+    category: string,
+    priority: number,
+    progress: number,
+    status: string,
+    active: number,
+    createdAt: number
+}
